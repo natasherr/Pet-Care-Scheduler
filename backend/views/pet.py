d44c38fb5906ed957fe730dd3d42f8218181c3e7
Ashley Natasha <ashleyotsiula@gmail.com>
2025-01-28 20:23:21 +0300

    Making changes to User Routes and Models

--- backend/views/pet.py
+++ backend/views/pet.py
@@ -1,6 +1,6 @@
 from models import db, Pet
-from flask import jsonify, request, Blueprint
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import jsonify, request, Blueprint # type: ignore
+from flask_jwt_extended import jwt_required, get_jwt_identity # type: ignore
 
 pet_bp = Blueprint('pet', __name__) 
 
